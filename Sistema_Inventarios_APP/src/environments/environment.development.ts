7bc280506bb36fcf188da38adf34da862e761eba
Juan Echeverria <juan.echeverriatro@ug.edu.ec>
2023-07-08 01:59:09 -0500

    Publicacion de Frontend a Hosting

--- Sistema_Inventarios_APP/src/environments/environment.development.ts
+++ Sistema_Inventarios_APP/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    endpoint: "http://localhost:5236/api/"
+    //endpoint: "http://localhost:5236/api/"
+    endpoint: "https://www.sistemainventarios.somee.com/api/"
 };
--- Sistema_Inventarios_APP/src/environments/environment.ts
+++ Sistema_Inventarios_APP/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    endpoint: "http://localhost:5236/api/"
+    endpoint: "https://www.sistemainventarios.somee.com/api/"
 };
